24eafa3273067b85844d1a3f25c7504267191b97
zihad07 <zihadnayem8gmail.com>
2019-10-09 19:09:59 +0600

    Basic file setup

---
+++ WorldBank/public/staff/index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>WBI</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../stylesheets/staff.css">
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+                <div class="col">
+                    <header>
+                        <h1>WBI Staff Area</h1>
+                    </header>
+
+                    <nav>
+                        <ul>
+                            <li><a href="index.php">Menu</a></li>
+                        </ul>
+                    </nav>
+                </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div id="content"></div>
+            </div>
+
+            <footer>
+                &copy; <?php echo date('Y');?> World Bank</footer>
+            </footer>
+        </div>
+
+        
+    </div>
+
+    
+    
+    <!-- JS files: jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
